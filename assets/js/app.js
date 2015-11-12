99739089b9abf3d9db7407e6e889057ed2baca44
Daniele Scasciafratte <mte90net@gmail.com>
2015-11-12 14:55:18 +0100

    added counter

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,43 +8,44 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 function create_map() {
   window.map = window.L.map('map', {
-	center: [20.0, 5.0],
-	minZoom: 2,
-	zoom: 2
+    center: [20.0, 5.0],
+    minZoom: 2,
+    zoom: 2
   });
   window.L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a>',
-	subdomains: ['otile1', 'otile2', 'otile3', 'otile4']
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a>',
+    subdomains: ['otile1', 'otile2', 'otile3', 'otile4']
   }).addTo(window.map);
   add_leaders();
 }
 
 function add_leaders() {
   window.fetch('leaders.json')
-		  .then(function (response) {
-			return response.json();
-		  }).
-		  then(function (json) {
-			json = json.leaders;
-			console.log(json.length + ' Leaders loaded');
-			for (var i = 0; i < json.length; ++i) {
-			  //add marker
-			  var popup = '<img src="' + get_gravatar_image_url(json[i].gravatar) + '" />';
-			  popup += '<br>' + json[i].country + '<br>';
-			  popup += '<a href="' + json[i].url + '" target="_blank">' + json[i].name + '</a>';
-			  window.L.marker([json[i].lat, json[i].lng])
-					  .bindPopup(popup)
-					  .addTo(window.map);
-			  //populate list
-			  var rowCount = window.table.rows.length;
-			  var row = window.table.insertRow(rowCount);
-			  var cell1 = row.insertCell(0);
-			  cell1.innerHTML = '<a href="' + json[i].url + '" target="_blank">' + json[i].name + '</a>';
-			  var cell2 = row.insertCell(1);
-			  cell2.innerHTML = json[i].country;
-			}
-			sorttable.makeSortable(newTableObject);
-		  });
+          .then(function (response) {
+            return response.json();
+          }).
+          then(function (json) {
+            json = json.leaders;
+            console.log(json.length + ' Leaders loaded');
+            document.querySelector('.counter').innerHTML = json.length + ' ';
+            for (var i = 0; i < json.length; ++i) {
+              //add marker
+              var popup = '<img src="' + get_gravatar_image_url(json[i].gravatar) + '" />';
+              popup += '<br>' + json[i].country + '<br>';
+              popup += '<a href="' + json[i].url + '" target="_blank">' + json[i].name + '</a>';
+              window.L.marker([json[i].lat, json[i].lng])
+                      .bindPopup(popup)
+                      .addTo(window.map);
+              //populate list
+              var rowCount = window.table.rows.length;
+              var row = window.table.insertRow(rowCount);
+              var cell1 = row.insertCell(0);
+              cell1.innerHTML = '<a href="' + json[i].url + '" target="_blank">' + json[i].name + '</a>';
+              var cell2 = row.insertCell(1);
+              cell2.innerHTML = json[i].country;
+            }
+            sorttable.makeSortable(newTableObject);
+          });
 }
 
 //from http://jsfiddle.net/xanderiel/Tvnq7/4/
